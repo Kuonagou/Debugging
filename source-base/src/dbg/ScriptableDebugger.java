71e4e16da3cb38c3a75de0cfaa68de3847120671
Kuonagou <anouk.gouhier@gmail.com>
2025-02-07 11:02:10 +0100

    step back fonctionne

--- source-base/src/dbg/ScriptableDebugger.java
+++ source-base/src/dbg/ScriptableDebugger.java
@@ -8,22 +8,27 @@ import com.sun.jdi.connect.VMStartException;
 import com.sun.jdi.event.*;
 import com.sun.jdi.request.BreakpointRequest;
 import com.sun.jdi.request.ClassPrepareRequest;
-import com.sun.jdi.request.StepRequest;
 import dbg.commands.CommandManager;
 import dbg.commands.Icommande;
-import dbg.commands.Step;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class ScriptableDebugger {
 
+    private Icommande commande;
     private Class debugClass;
     private VirtualMachine vm;
     private CommandManager commandManager = new CommandManager();
+    private boolean askForStepBack = false;
+    private int stepBack ;
+    private List<Integer> saveBreakPoints;
+
 
     public VirtualMachine connectAndLaunchVM() throws IOException, IllegalConnectorArgumentsException, VMStartException {
         LaunchingConnector launchingConnector = Bootstrap.virtualMachineManager().defaultConnector();
@@ -76,9 +81,21 @@ public class ScriptableDebugger {
                         System.out.println("Target VM input stream reading error.");
                     }
                 }
-                if(event instanceof ClassPrepareEvent) { // break point à l'instanciation
-                    setBreakPoint(debugClass.getName() , 6 ) ;
-                    setBreakPoint(debugClass.getName() , (25) ) ;
+                if(event instanceof ClassPrepareEvent) {
+                    if(askForStepBack){
+                        System.out.println("J'ajoute mon breakpoint ligne "+stepBack);
+                        setBreakPoint(debugClass.getName(),stepBack,true);
+                        for (Integer line : saveBreakPoints){
+                            System.out.println(line);
+                            setBreakPoint(debugClass.getName(),line,line > stepBack);
+                            System.out.println(line);
+
+                        }
+                    }else{
+                        setBreakPoint(debugClass.getName() , 6,true ) ;
+                        setBreakPoint(debugClass.getName() , 25 ,true) ;
+                    }
+
                 }
                 if(event instanceof BreakpointEvent | event instanceof StepEvent) {
                     readInput((LocatableEvent) event);
@@ -88,28 +105,68 @@ public class ScriptableDebugger {
         }
     }
 
-    private void setBreakPoint(String className, int lineNumber) throws AbsentInformationException {
+    private void setBreakPoint(String className, int lineNumber,boolean actif) throws AbsentInformationException {
         for( ReferenceType targetClass : vm.allClasses()) {
             if(targetClass.name().equals(className)){
                 Location location = targetClass.locationsOfLine(lineNumber).get(0) ;
                 BreakpointRequest bpReq = vm. eventRequestManager().createBreakpointRequest(location);
-                bpReq.enable();
+                if(actif){
+                    bpReq.enable();
+                }else{
+                    bpReq.disable();
+                }
+
             }
         }
     }
 
     private void readInput(LocatableEvent event) throws Exception {
         try {
+            int ligneActuelle = event.location().lineNumber();
+            System.out.println("Ligne de l'event "+ligneActuelle);
             boolean reconnue = false;
             BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
             while(!reconnue){
                 System.out.print("Entrez une commande > ");
                 String command = reader.readLine();
-                reconnue =  commandManager.executeCommand(vm, event, command);
+
+                if(command.equals("step back")){
+                    stepBack = ligneActuelle - 1;
+                    System.out.println("step back"+stepBack);
+                    stepBack();
+                }else if (command.equals("step back for")){
+                    System.out.print("Nombre de step back > ");
+                    command = reader.readLine();
+                    stepBack = ligneActuelle - Integer.parseInt(command);
+                    System.out.println("step back"+stepBack);
+                    stepBack();
+                }else{
+                    reconnue =  commandManager.executeCommand(vm, event, command);
+                }
+
             }
 
         } catch (Exception e) {
             throw new Exception(e.getMessage());
+        }
+    }
+
+    private void stepBack() throws AbsentInformationException {
+        saveBreakpoints();
+        askForStepBack = true;
+        //ajouter de gerer qu'on ne sort pas des index prévus
+        attachTo(JDISimpleDebuggee.class);
+    }
+
+    private void saveBreakpoints() throws AbsentInformationException {
+        List<BreakpointRequest> breakpoints = vm.eventRequestManager().breakpointRequests();
+        saveBreakPoints = new ArrayList<>();
+        if (breakpoints.isEmpty()) {
+            System.out.println("Pas de breakpoints.");
+        } else {
+            for (BreakpointRequest bp : breakpoints) {
+                saveBreakPoints.add(bp.location().lineNumber());
+            }
         }
     }
 
